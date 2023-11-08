51c1f06c0e0bb9c693e6d78eb0c37cc3f562a0d4
akhsak <akhsamariyam2k2 @gmail.com>
2023-11-08 13:53:10 +0530

    add

---
+++ lib/screens/Explore.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Explorepage extends StatelessWidget {
+  const Explorepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('explore'),
+    );
+  }
+}
---
+++ lib/screens/categories.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Categoriespage extends StatelessWidget {
+  const Categoriespage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('cate'),
+    );
+  }
+}
